42668f004009c330a6fec388fc8af251002ef461
Santhosh Reddy <santhoshbeeram19@gmail.com>
2025-12-20 12:50:53 +0530

    Added isAbove18 fn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const age = 24;
-const name = "Santhosh"
+const name1 = "Santhosh"
 const dob = "19/11/2004"
 
 
 function sayName(){
-    console.log("My Name is "+name);
+    console.log("My Name is "+name1);
+}
+
+function isAboove18(){
+    return age>18;
 }
